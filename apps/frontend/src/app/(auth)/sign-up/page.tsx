9c5e2f352f843f08eb3f4d2b962abdc50894862e
Krish Kalaria <codewithkrish12@gmail.com>
2025-07-29 23:01:46 +0530

    feat: add auth ui

---
+++ apps/frontend/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view'
+
+const SignUp = () => {
+  return (
+    <SignUpView />
+  )
+}
+
+export default SignUp
