38bbd1e31f9483a621620cf48c153ffdf21c4894
Krish Kalaria <codewithkrish12@gmail.com>
2025-07-30 23:36:25 +0530

    feat: add responsive dialog and command dialog

--- apps/frontend/src/modules/dashboard/ui/components/dashboard-command.tsx
+++ apps/frontend/src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,6 +1,11 @@
 import { Dispatch, SetStateAction } from "react";
 
-import { CommandDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { 
+  CommandInput, 
+  CommandItem, 
+  CommandList, 
+  CommandResponsiveDialog 
+} from "@/components/ui/command";
 
 interface Props {
   open: boolean;
@@ -9,11 +14,11 @@ interface Props {
 
 export const DashBoardCommand = ({ open, setOpen }: Props) => {
   return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
+    <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
       <CommandInput placeholder="Find a interview" />
       <CommandList>
         <CommandItem>Test</CommandItem>
       </CommandList>
-    </CommandDialog>
+    </CommandResponsiveDialog>
   );
 };
